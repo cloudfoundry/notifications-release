3bf3387808a8da7ccd919ea33dc9f464e26766a4
Ian Ornstein and Ryan Moran <iornstein@pivotal.io>
2014-10-15 11:24:13 -0700

    Acceptance Tests now print output for debugging
    [#80169406]

--- acceptance/send_email_to_user_and_space_test.go
+++ acceptance/send_email_to_user_and_space_test.go
@@ -3,131 +3,110 @@ package acceptance_test
 import (
     "encoding/json"
     "fmt"
-    "os"
-    "os/exec"
     "strings"
+
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
 )
 
-type GUIDResponse struct {
-    Resources []struct {
-        MetaData struct {
-            GUID string `json:guid`
-        } `json:metadata`
-    }
-}
-
 var _ = Describe("SendEmailToUserAndSpace", func() {
-    var uaacUserID string
-    var orgGUIDResponse GUIDResponse
-    var spaceGUIDResponse GUIDResponse
-    var uaacAdminClientUserName string
-    var uaacAdminClientPassword string
+    var context TestSuiteContext
 
     BeforeSuite(func() {
-        // LOAD ENVIRONMENT LOGIN INFO
+        // LOAD ENVIRONMENT AND CONTEXT
+        context = TestSuiteContext{
+            TestUserName:                Randomized("user"),
+            TestUserPassword:            Randomized("password"),
+            TestOrg:                     Randomized("org"),
+            TestSpace:                   Randomized("space"),
+            TestClientSenderID:          Randomized("client"),
+            TestClientSenderSecret:      Randomized("secret"),
+            TestClientSenderAuthorities: "notifications.write",
+            TestClientSenderGrantTypes:  "client_credentials",
 
-        uaacAdminClientUserName = os.Getenv("UAAC_ADMIN_CLIENT_USERNAME")
-        uaacAdminClientPassword = os.Getenv("UAAC_ADMIN_CLIENT_PASSWORD")
-        cfAdminUsername := os.Getenv("CF_ADMIN_USERNAME")
-        cfAdminPassword := os.Getenv("CF_ADMIN_PASSWORD")
+            UAACAdminClientID:       LoadOrPanic("UAAC_ADMIN_CLIENT_USERNAME"),
+            UAACAdminClientPassword: LoadOrPanic("UAAC_ADMIN_CLIENT_PASSWORD"),
+            CFAdminUsername:         LoadOrPanic("CF_ADMIN_USERNAME"),
+            CFAdminPassword:         LoadOrPanic("CF_ADMIN_PASSWORD"),
+            NotificationsDomain:     LoadOrPanic("NOTIFICATIONS_DOMAIN"),
+            UAADomain:               LoadOrPanic("UAA_DOMAIN"),
+            CCDomain:                LoadOrPanic("CC_DOMAIN"),
+        }
 
         // LOGIN AS A CF USER
-        command := exec.Command("cf", "login", "-u", cfAdminUsername, "-p", cfAdminPassword)
-        err := command.Run()
-        if err != nil {
-            panic("Couldn't login to CF, check your credentials")
-        }
+        Run("cf", "api", context.CCDomain, "--skip-ssl-validation")
+        Run("cf", "login", "-u", context.CFAdminUsername, "-p", context.CFAdminPassword)
 
         // CREATE A USER AND GRAB ITS TOKEN
-        command = exec.Command("cf", "create-user", "notificationsTestUser", "password")
-        command.Run()
-        command = exec.Command("cf", "create-org", "notificationsTestOrg")
-        command.Run()
-        command = exec.Command("cf", "create-space", "notificationsTestSpace", "-o", "notificationsTestOrg")
-        command.Run()
+        Run("cf", "create-user", context.TestUserName, context.TestUserPassword)
+        Run("cf", "create-org", context.TestOrg)
+        Run("cf", "create-space", context.TestSpace, "-o", context.TestOrg)
+        Run("cf", "target", "-o", context.TestOrg, "-s", context.TestSpace)
+        Run("uaac", "target", context.UAADomain)
+        Run("uaac", "token", "client", "get", context.UAACAdminClientID, "-s", context.UAACAdminClientPassword)
 
-        command = exec.Command("uaac", "token", "client", "get", uaacAdminClientUserName, "-s", uaacAdminClientPassword)
-        err = command.Run()
-        if err != nil {
-            panic("Coudln't login to UAAC, check your credentials")
-        }
-
-        command = exec.Command("uaac", "user", "get", "notificationsTestUser", "-a", "id")
-        data, _ := command.Output()
-        uaacUserID = strings.Split(string(data), ":")[1]
-        uaacUserID = strings.TrimSpace(uaacUserID)
+        output := Run("uaac", "user", "get", context.TestUserName, "-a", "id")
+        context.TestUserGUID = strings.TrimSpace(strings.Split(output, ":")[1])
 
         // GET A CLIENT WITH THE RIGHT SCOPES
-        command = exec.Command("uaac", "client", "add", "notifications-sender", "--scope", "notifications.write", "--authorities",
-            "notifications.write", "-s", "secret", "--authorized_grant_types", "client_credentials")
-        command.Run()
-        command = exec.Command("uaac", "token", "client", "get", "notifications-sender", "-s", "secret")
-        command.Run()
+        Run("uaac", "client", "add", context.TestClientSenderID, "--authorities", context.TestClientSenderAuthorities, "-s", context.TestClientSenderSecret, "--authorized_grant_types", context.TestClientSenderGrantTypes)
+        Run("uaac", "token", "client", "get", context.TestClientSenderID, "-s", context.TestClientSenderSecret)
     })
 
     AfterSuite(func() {
-        command := exec.Command("cf", "delete-user", "notificationsTestUser", "-f")
-        command.Run()
-        command = exec.Command("cf", "delete-space", "notificationsTestSpace", "-f")
-        command.Run()
-        command = exec.Command("cf", "delete-org", "notificationsTestOrg", "-f")
-        command.Run()
-
-        command = exec.Command("uaac", "token", "client", "get", uaacAdminClientUserName, "-s", uaacAdminClientPassword)
-        command.Run()
-
-        command = exec.Command("uaac", "client", "delete", "notifications-sender")
-        command.Run()
+        AlwaysRun("cf", "login", "-u", context.CFAdminUsername, "-p", context.CFAdminPassword)
+        AlwaysRun("cf", "target", "-o", context.TestOrg, "-s", context.TestSpace)
+        AlwaysRun("cf", "delete-user", context.TestUserName, "-f")
+        AlwaysRun("cf", "delete-space", context.TestSpace, "-f")
+        AlwaysRun("cf", "delete-org", context.TestOrg, "-f")
+        AlwaysRun("uaac", "token", "client", "get", context.UAACAdminClientID, "-s", context.UAACAdminClientPassword)
+        AlwaysRun("uaac", "client", "delete", context.TestClientSenderID)
     })
 
     Describe("sending an email to a user", func() {
         It("returns a 200", func() {
             // SEND A NOTIFICATION TO A USER
-            notificationToUserURL := fmt.Sprintf("%s/users/%s", os.Getenv("NOTIFICATIONS_DOMAIN"), uaacUserID)
-            command := exec.Command("uaac", "curl", notificationToUserURL, "-X", "POST", "--data", `{"kind_id":"test_notification", "text":"this is a test"}`)
-            data, _ := command.Output()
+            notificationToUserURL := fmt.Sprintf("%s/users/%s", context.NotificationsDomain, context.TestUserGUID)
+            output := Run("uaac", "curl", notificationToUserURL, "-X", "POST", "--data", `{"kind_id":"test_notification", "text":"this is a test"}`)
 
             // VERIFY 200 RESPONSE
-            Expect(data).To(ContainSubstring("200 OK"))
+            Expect(output).To(ContainSubstring("200 OK"))
+            Expect(output).To(ContainSubstring(`"recipient":"` + context.TestUserGUID + `"`))
         })
     })
 
     Describe("sending an email to a space", func() {
         It("returns a 200", func() {
             // PUT A USER IN A SPACE
-            command := exec.Command("cf", "set-space-role", "notificationsTestUser", "notificationsTestOrg", "notificationsTestSpace", "SpaceDeveloper")
-            command.Run()
-            command = exec.Command("cf", "login", "-u", "notificationsTestUser", "-p", "password")
-            command.Run()
+            Run("cf", "set-space-role", context.TestUserName, context.TestOrg, context.TestSpace, "SpaceDeveloper")
+            Run("cf", "login", "-u", context.TestUserName, "-p", context.TestUserPassword)
 
             // GRAB IDs FOR CURL REQUESTS
-            command = exec.Command("cf", "curl", "/v2/organizations")
-            data, _ := command.Output()
-            err := json.Unmarshal(data, &orgGUIDResponse)
+            output := Run("cf", "curl", "/v2/organizations")
+
+            var orgGUIDResponse GUIDResponse
+            err := json.Unmarshal([]byte(output), &orgGUIDResponse)
             if err != nil {
                 panic(err)
             }
-
             orgGUID := orgGUIDResponse.Resources[0].MetaData.GUID
 
-            command = exec.Command("cf", "curl", fmt.Sprintf("/v2/organizations/%s/spaces", orgGUID))
-            data, _ = command.Output()
-            err = json.Unmarshal(data, &spaceGUIDResponse)
+            output = Run("cf", "curl", fmt.Sprintf("/v2/organizations/%s/spaces", orgGUID))
+
+            var spaceGUIDResponse GUIDResponse
+            err = json.Unmarshal([]byte(output), &spaceGUIDResponse)
             if err != nil {
                 panic(err)
             }
             spaceGUID := spaceGUIDResponse.Resources[0].MetaData.GUID
 
             // SEND A NOTIFICATION TO A SPACE
-            notificationToSpaceURL := fmt.Sprintf("%s/spaces/%s", os.Getenv("NOTIFICATIONS_DOMAIN"), spaceGUID)
-            command = exec.Command("uaac", "curl", notificationToSpaceURL, "-X", "POST", "--data", `{"kind_id":"test_notification", "text":"this is a test"}`)
-            data, _ = command.Output()
+            notificationToSpaceURL := fmt.Sprintf("%s/spaces/%s", context.NotificationsDomain, spaceGUID)
+            output = Run("uaac", "curl", notificationToSpaceURL, "-X", "POST", "--data", `{"kind_id":"test_notification", "text":"this is a test"}`)
 
             // VERIFY 200 RESPONSE
-            Expect(data).To(ContainSubstring("200 OK"))
+            Expect(output).To(ContainSubstring("200 OK"))
+            Expect(output).To(ContainSubstring(`"recipient":"` + context.TestUserGUID + `"`))
         })
     })
-
 })
---
+++ acceptance/support.go
@@ -0,0 +1,80 @@
+package acceptance
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+    "strings"
+
+    "github.com/nu7hatch/gouuid"
+)
+
+type TestSuiteContext struct {
+    UAACAdminClientID           string
+    UAACAdminClientPassword     string
+    CFAdminUsername             string
+    CFAdminPassword             string
+    TestUserGUID                string
+    TestUserName                string
+    TestUserPassword            string
+    TestOrg                     string
+    TestSpace                   string
+    TestClientSenderID          string
+    TestClientSenderSecret      string
+    TestClientSenderAuthorities string
+    TestClientSenderGrantTypes  string
+    NotificationsDomain         string
+    UAADomain                   string
+    CCDomain                    string
+}
+
+type GUIDResponse struct {
+    Resources []struct {
+        MetaData struct {
+            GUID string `json:guid`
+        } `json:metadata`
+    }
+}
+
+func AlwaysRun(command string, arguments ...string) {
+    defer func() {
+        err := recover()
+        if err != nil {
+            fmt.Printf("%+v\n", err)
+        }
+    }()
+
+    Run(command, arguments...)
+}
+
+func Run(command string, arguments ...string) string {
+    parts := []string{"$", command}
+    parts = append(parts, arguments...)
+    fmt.Println(strings.Join(parts, " "))
+
+    cmd := exec.Command(command, arguments...)
+    output, err := cmd.CombinedOutput()
+    fmt.Println(string(output))
+    if err != nil {
+        panic(err)
+    }
+
+    return string(output)
+}
+
+func LoadOrPanic(variable string) string {
+    value := os.Getenv(variable)
+    if value == "" {
+        panic(variable + " is a required environment variable")
+    }
+    return value
+}
+
+func Randomized(prefix string) string {
+    entropy, err := uuid.NewV4()
+    if err != nil {
+        panic(err)
+    }
+
+    return prefix + "-" + entropy.String()
+}
